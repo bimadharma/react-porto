ce84654d15751ca210d8ed9eed242fff8b7e692b
bimadharma <bimadharmayahya@gmail.com>
2025-02-09 01:10:22 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./containers/home";
 import About from "./containers/about"
@@ -7,11 +7,21 @@ import Portfolio from "./containers/portfolio"
 import Contact from "./containers/contact"
 import Skills from "./containers/skills"
 import Navbar from "./components/navbar";
+import ParticlesComponent from './utils.js/ParticlesComponent';
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+
+  const renderParticleJsIHomePage = location.pathname === "/";
+
+
   return (
     <div className="App">
       {/* particles js */}
+
+      {renderParticleJsIHomePage && <ParticlesComponent/>}
+      
 
       {/* navbar */}
       <Navbar/>
---
+++ src/utils.js/ParticlesComponent.jsx
@@ -0,0 +1,101 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState, useCallback } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesComponent = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesOptions = {
+    autoPlay: true,
+    background: {
+      color: {
+        value: "#1d1d1d",
+      },
+    },
+    fullScreen: {
+      enable: true,
+      zIndex: -1,
+    },
+    detectRetina: true,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push", // Mode klik default
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+      },
+      modes: {
+        push: {
+          quantity: 2, // Jumlah partikel yang keluar setiap klik
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ff0000",
+        animation: {
+          h: {
+            enable: true,
+            speed: 20,
+            sync: false,
+          },
+        },
+      },
+      links: {
+        enable: false,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        outModes: {
+          default: "out",
+        },
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 50, // Partikel default
+      },
+      opacity: {
+        value: 0.8,
+      },
+      shape: {
+        type: ["circle", "square"],
+      },
+      size: {
+        value: {
+          min: 10,
+          max: 20,
+        },
+      },
+      shadow: {
+        enable: true,
+        blur: 10,
+        color: "#ffffff",
+        offset: {
+          x: 3,
+          y: 3,
+        },
+      },
+    },
+  };
+
+  return init ? <Particles options={particlesOptions} /> : null;
+};
+
+export default ParticlesComponent;
